8c0346bb825771624004dcf3fa79d62b3e3ac2ce
Subhasish Das <anita80subhasish@gmail.com>
2025-05-04 23:48:39 +0530

    providerlandingpages done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
-// src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePageMock from './Pages/HomePageMock';
-import Login from './Pages/Login';
+import Login from './Pages/login/Login';
 import LandingPage from './Pages/Signup/LandingPage';
 import SignupUser from './Pages/Signup/SignupUser';
 import SignupProvider from './Pages/Signup/SignupProvider';
+import Hero from './Pages/ServiceLandingpages/Hero.jsx';
 
-import ProtectedRoute from './utils/ProtectedRoute'; 
+import ProtectedRoute from './utils/ProtectedRoute';
 
 const App = () => {
   return (
@@ -18,7 +18,8 @@ const App = () => {
         <Route path="/signup" element={<LandingPage />} />
         <Route path="/signup/user" element={<SignupUser />} />
         <Route path="/signup/provider" element={<SignupProvider />} />
-        
+        <Route path="/hero" element={<Hero />} />
+       
       </Routes>
     </Router>
   );
